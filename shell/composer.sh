419a3f611182fd57990cee0f0c1ecdd02a11f88c
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2021-04-08 10:23:11 -0400

    Add composer configurations

---
+++ shell/composer.sh
@@ -0,0 +1,5 @@
+# Time Composer waits for commands (such as git commands) to finish executing
+export COMPOSER_PROCESS_TIMEOUT=3600
+
+# The value is used as php's memory_limit.
+export COMPOSER_MEMORY_LIMIT=-1
